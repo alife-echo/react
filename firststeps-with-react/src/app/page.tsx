b0c556e29ac9955033b82c9856042b0deedec742
alife-echo <alifesilva90@gmail.com>
2023-08-16 09:23:08 -0300

    init steps react

---
+++ firststeps-with-react/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page (){
+   return (
+      <> 
+       <h1 className="font-bold text-lg bg-blue-600 text-white" >Olá Mundo</h1>
+       <h3>Algum outro texto</h3>
+      </>
+   )
+}
