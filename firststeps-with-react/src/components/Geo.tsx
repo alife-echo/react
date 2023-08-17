ed7f059aa3e4f289e4e1e2d339e566a6e0a84165
alife-echo <alifesilva90@gmail.com>
2023-08-17 11:25:21 -0300

    import and export in react

---
+++ firststeps-with-react/src/components/Geo.tsx
@@ -0,0 +1,14 @@
+export function Square () {
+    return (
+      <div className="w-52 h-52 bg-gray-600 text-white font-bold flex items-center justify-center">
+         Texto qualquer inspirador
+      </div>
+    )
+}
+export function Circle () {
+    return (
+      <div className="w-52 h-52 bg-blue-600 text-white font-bold flex items-center justify-center rounded-full">
+         Texto qualquer inspirador
+      </div>
+    )
+}
