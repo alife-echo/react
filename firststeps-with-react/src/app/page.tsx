8e84807f3a13c663efb364cca29c592752b1e1b6
alife-echo <alifesilva90@gmail.com>
2023-08-17 13:28:25 -0300

    rules jsx

--- firststeps-with-react/src/app/page.tsx
+++ firststeps-with-react/src/app/page.tsx
@@ -1,4 +1,5 @@
 import { GeoForm } from "@/components/GeoForm";
+import { Person } from "@/components/Person";
 
 export default function Page (){
    return (
@@ -6,6 +7,7 @@ export default function Page (){
        <h1 className="font-bold text-lg bg-blue-600 text-white" >Olá Mundo</h1>
        <h3>Algum outro texto</h3>
        <GeoForm/>
+       <Person/>
       </>
    )
 }
---
+++ firststeps-with-react/src/components/Person.tsx
@@ -0,0 +1,17 @@
+export function Person () {
+    return (
+        <>
+            <h1>Edward Kenway</h1>
+            <img 
+                src='https://cdn.ome.lt/LlY5oiP_AKVDbHmj1nbGmglYmic=/770x0/smart/uploads/conteudo/fotos/Assassins_Creed_Edward.jpg'
+                alt='Edward Kenway' 
+                className="w-52 h-52 hover:scale-110 transition-all"
+            /> 
+            <ul>
+                <li>Ceo da Tesla</li>
+                <li>Ceo da Spacex</li>
+                <li>Ceo da ......</li>
+            </ul>
+        </>
+    )
+}
